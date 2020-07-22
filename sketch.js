bcf43217deda268060f3adaff17c324894b098a2
kiwi55528 <steven11344200@gmail.com>
2020-07-21 20:09:42 -0700

    added a chain and put it on a bird

--- sketch.js
+++ sketch.js
@@ -2,10 +2,12 @@ const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
 
+const Constraint=Matter.Constraint
 var engine, world;
 var box1, pig1;
 var backgroundImg,platform;
-
+var log10;
+var sling;
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
 }
@@ -35,7 +37,8 @@ function setup(){
     log5 = new Log(870,120,150, -PI/7);
 
     bird = new Bird(100,100);
-
+    log10 =  new Log(200,200,150, PI/2);
+    sling= new Chain(bird.body,log10.body)
 }
 
 function draw(){
@@ -58,7 +61,8 @@ function draw(){
     box5.display();
     log4.display();
     log5.display();
-
+    sling.display();
+    log10.display();
     bird.display();
     platform.display();
 }
